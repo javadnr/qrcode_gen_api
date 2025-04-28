1908cc21fb377883f16672c5d03faa3b76ac4495
javadnr <javadnamavar554@gmail.com>
2025-04-28 11:58:34 +0330

    changed route and removed qrcode from it

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 
-app.include_router(generate.router, prefix="/api/v1/qrcode", tags=["QRCode"])
+app.include_router(generate.router, prefix="/api/v1", tags=["QRCode"])
